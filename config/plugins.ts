3ea1fb67c4a16d20c39c4506f2c30737dddb822d
Ricardo Santoyo <ricardo.santoyo@hotmail.com>
2024-10-17 14:16:36 -0400

    typescript generated

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,14 @@
-export default () => ({});
+export default () => ({
+  "schemas-to-ts": {
+    enabled: true,
+    config: {
+        acceptedNodeEnvs: ["development"],
+        commonInterfacesFolderName: 'schemas-to-ts',
+        alwaysAddEnumSuffix: false,
+        alwaysAddComponentSuffix: false,
+        usePrettierIfAvailable: true,
+        logLevel: 2,
+        destinationFolder: 'generated-interfaces',
+      }
+  },
+});
